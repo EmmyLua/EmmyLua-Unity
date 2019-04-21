5752f065c5a7b84a598911322ad83c36324844c4
tangzx <love.tangzx@qq.com>
2019-04-21 19:57:03 +0800

    update

--- Unity/Assets/Editor/EmmyLuaService.cs
+++ Unity/Assets/Editor/EmmyLuaService.cs
@@ -28,9 +28,9 @@ namespace EmmyLua
 	{
 		private static Socket socket;
 
-		private static Thread reciveThread;
+		private static Thread receiveThread;
 
-		private static int PORT = 9988;
+		private static int PORT = 996;
 
 		private static bool doTryLater;
 		
@@ -38,16 +38,55 @@ namespace EmmyLua
 
 		private static bool connected;
 
+		private static bool running;
+
+		private const string KEY_EMMY_SERVICE_ENABLE = "emmy.service.enable";
+
+		private static bool IsEnable
+		{
+			get { return EditorPrefs.GetBool(KEY_EMMY_SERVICE_ENABLE); }
+			set { EditorPrefs.SetBool(KEY_EMMY_SERVICE_ENABLE, value); }
+		}
+
+		[MenuItem("EmmyLua/Enable")]
+		static void EnableService()
+		{
+			IsEnable = true;
+			StartConnect();
+		}
+		
+		[MenuItem("EmmyLua/Enable", true)]
+		static bool EnableServiceCheck()
+		{
+			return !IsEnable;
+		}
+		
+		[MenuItem("EmmyLua/Disable")]
+		static void DisableService()
+		{
+			IsEnable = false;
+			Stop();
+		}
+		
+		[MenuItem("EmmyLua/Disable", true)]
+		static bool DisableServiceCheck()
+		{
+			return IsEnable;
+		}
+
 		static EmmyLuaService()
 		{
 			EditorApplication.update += Update;
-			BeginConnect();
+			StartConnect();
 		}
 
-		static void BeginConnect()
+		static void StartConnect()
 		{
-			doTryLater = false;
+			if (running || !IsEnable)
+				return;
+			running = true;
 			connected = false;
+			doTryLater = false;
 			try
 			{
 				if (socket != null)
@@ -77,13 +116,29 @@ namespace EmmyLua
 
 		private static void TryLater()
 		{
+			running = false;
 			connected = false;
 			doTryLater = true;
 			lastTime = DateTime.Now;
 		}
 
+		private static void Stop()
+		{
+			if (running)
+			{
+				running = false;
+				connected = false;
+				doTryLater = false;
+				
+				if (socket != null)
+					socket.Close();
+			}
+		}
+
 		private static void Update()
 		{
+			if (!IsEnable)
+				return;
 			var sp = DateTime.Now - lastTime;
 			if (sp.TotalSeconds > 5)
 			{
@@ -93,7 +148,7 @@ namespace EmmyLua
 				}
 				else if (doTryLater)
 				{
-					BeginConnect();
+					StartConnect();
 				}
 			}
 		}
@@ -101,7 +156,7 @@ namespace EmmyLua
 		[DidReloadScripts]
 		static void UpdateScripts()
 		{
-			BeginConnect();
+			StartConnect();
 		}
 
 		private static void WriteString(BinaryWriter writer, string value)
@@ -153,68 +208,7 @@ namespace EmmyLua
 			var types = GetTypes();
 			foreach (var type in types)
 			{
-				var fullName = type.FullName;
-				if (!string.IsNullOrEmpty(fullName))
-				{
-					// full name
-					WriteString(writer, fullName);
-					
-					// base type full name
-					{
-						string baseTypeFullName = null;
-						if (type.BaseType != null)
-							baseTypeFullName = type.BaseType.FullName;
-						writer.Write(baseTypeFullName != null);
-						if (baseTypeFullName != null)
-							WriteString(writer, baseTypeFullName);
-					}
-
-					// fields
-					var fields =
-						type.GetFields(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly);
-					writer.Write(fields.Length);
-					foreach (var fi in fields)
-					{
-						WriteString(writer, fi.Name);
-						WriteType(writer, fi.FieldType);
-					}
-
-					// properties
-					var properties =
-						type.GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly);
-					writer.Write(properties.Length);
-					foreach (var pi in properties)
-					{
-						WriteString(writer, pi.Name);
-						WriteType(writer, pi.PropertyType);
-					}
-
-					// methods
-					var methods =
-						(from mi in type.GetMethods(BindingFlags.Public | BindingFlags.Instance |
-						                            BindingFlags.DeclaredOnly)
-							where !mi.Name.StartsWith("get_") && !mi.Name.StartsWith("set_")
-							select mi).ToArray();
-
-					writer.Write(methods.Count());
-					foreach (var mi in methods)
-					{
-						// name
-						WriteString(writer, mi.Name);
-
-						// parameters
-						var parameterInfos = mi.GetParameters();
-						writer.Write(parameterInfos.Length);
-						foreach (var pi in parameterInfos)
-						{
-							WriteString(writer, pi.Name);
-							WriteType(writer, pi.ParameterType);
-						}
-
-						// returns
-						WriteType(writer, mi.ReturnType);
-					}
-				}
+				WriteTypeData(type, writer);
 			}
 
 			writer.Flush();
@@ -229,17 +223,85 @@ namespace EmmyLua
 			writer.Close();
 		}
 
+		private static void WriteTypeData(Type type, BinaryWriter writer)
+		{
+			var fullName = type.FullName;
+			if (!string.IsNullOrEmpty(fullName))
+			{
+				// full name
+				WriteString(writer, fullName);
+
+				// base type full name
+				{
+					string baseTypeFullName = null;
+					if (type.BaseType != null)
+						baseTypeFullName = type.BaseType.FullName;
+					writer.Write(baseTypeFullName != null);
+					if (baseTypeFullName != null)
+						WriteString(writer, baseTypeFullName);
+				}
+
+				// fields
+				var fields =
+					type.GetFields(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly);
+				writer.Write(fields.Length);
+				foreach (var fi in fields)
+				{
+					WriteString(writer, fi.Name);
+					WriteType(writer, fi.FieldType);
+				}
+
+				// properties
+				var properties =
+					type.GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly);
+				writer.Write(properties.Length);
+				foreach (var pi in properties)
+				{
+					WriteString(writer, pi.Name);
+					WriteType(writer, pi.PropertyType);
+				}
+
+				// methods
+				var methods =
+					(from mi in type.GetMethods(BindingFlags.Public | BindingFlags.Instance |
+					                            BindingFlags.DeclaredOnly)
+						where !mi.Name.StartsWith("get_") && !mi.Name.StartsWith("set_")
+						select mi).ToArray();
+
+				writer.Write(methods.Count());
+				foreach (var mi in methods)
+				{
+					// name
+					WriteString(writer, mi.Name);
+
+					// parameters
+					var parameterInfos = mi.GetParameters();
+					writer.Write(parameterInfos.Length);
+					foreach (var pi in parameterInfos)
+					{
+						WriteString(writer, pi.Name);
+						WriteType(writer, pi.ParameterType);
+					}
+
+					// returns
+					WriteType(writer, mi.ReturnType);
+				}
+			}
+		}
+
 		private static Type[] GetTypes()
 		{
 			var unityTypes = from assembly in AppDomain.CurrentDomain.GetAssemblies()
 				where !(assembly.ManifestModule is ModuleBuilder)
+				      && assembly.FullName != "UnityEditor"
 				from type in assembly.GetExportedTypes()
-				where type.BaseType != typeof(MulticastDelegate) 
+				where type.BaseType != typeof(MulticastDelegate)
 				      && !type.IsInterface 
 				      && !type.IsEnum
+				      //&& !type.IsGenericType
+				      //&& !type.IsGenericTypeDefinition
+				      && !type.IsNested
 				      && !IsExcluded(type)
-				      //&& type.Namespace != null 
-				      //&& type.Namespace.StartsWith("UnityEngine") 
 				select type;
 			var arr = unityTypes.ToArray();
 
